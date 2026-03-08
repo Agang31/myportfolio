bce9b396c04159dc68da7d5363ec05921969c673
Agang31 <agangqanxwa@gmail.com>
2026-03-08 23:49:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -54,3 +54,17 @@ function typeEffect() {
     setTimeout(typeEffect, speed); 
 } 
 typeEffect(); 
+
+const backToTopButton = document.getElementById('back-to-top'); 
+
+window.addEventListener('scroll', function() { 
+    if (window.scrollY > 700) { 
+        backToTopButton.style.display = 'block'; 
+    } else { 
+        backToTopButton.style.display = 'none'; 
+    } 
+}); 
+
+backToTopButton.addEventListener('click', function() { 
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+}); 
